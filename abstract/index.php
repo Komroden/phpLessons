42eaff3e14adabe69b62151fb6cf3d0abc544cf9
Кирилл Незнанов <Komroden@gmail.com>
2022-01-31 14:57:03 +0300

    less2.3

--- abstract/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-$obj_digital = new WebGood(3);
-echo $obj_digital->profitCalc();
-
-$obj_real = new SimpleGood(3);
-echo $obj_real->profitCalc();
-
-$obj_weight = new WheightGood(3, 300);
-echo $obj_weight->profitCalc();
