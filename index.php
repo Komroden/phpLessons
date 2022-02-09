433b92c262bc504834ec205a0f211406619ce554
Кирилл Незнанов <Komroden@gmail.com>
2022-02-09 13:26:16 +0300

    lesson2/6

--- index.php
+++ index.php
@@ -1,17 +1,1 @@
-<?php
-gi
-require_once 'autoload.php';
-
-$method = (isset($_GET['method'])) ? $_GET['method'] : 'read'; 
-
-if (isset($_GET['class'])) {
-    if ($_GET['class'] === 'page') {
-        $controller = new PageC();
-    } elseif ($_GET['class'] === 'user') {
-        $controller = new UserC();
-    }
-} else {
-    $controller = new PageC();
-}
-
-$controller -> request($method);
+<?php	function __autoload($classname){		include_once("c/$classname.php");	}	$action = 'action_';	$action .= (isset($_GET['act'])) ? $_GET['act'] : 'index';		if (isset($_GET['id'])) {		$id = $_GET['id'];	} else {		$id = false;	}	if (isset($_GET['c'])) {		if ($_GET['c'] == 'page') {			$controller = new C_Page();		} else if ($_GET['c'] == 'user') {			$controller = new C_User();		} else if ($_GET['c'] == 'admin') {			$controller = new C_Admin();		}	} else {		$controller = new C_Page();	}	$controller->Request($action, $id);
